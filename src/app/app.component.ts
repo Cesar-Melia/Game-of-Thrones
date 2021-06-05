ffc1284b082a8bf7032bdd360065d9ac6c84019e
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-06-05 13:23:37 +0200

    app.component changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 // import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'game-of-thrones';
 
-  constructor(){}
+  detailPage: boolean = false;
 
+  constructor(private router: Router) {
+    router.events.subscribe((val) => {
+      if (window.location.href.includes('characters/' || 'houses/')) {
+        this.detailPage = true;
+      } else {
+        this.detailPage = false;
+      }
+
+      console.log('detailPage = ' + this.detailPage);
+    });
+  }
 }
