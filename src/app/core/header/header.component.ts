8542cb4dcdfdf057ddb0241990fd25d06398479f
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-06-05 19:54:10 +0200

    translate bug solved

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -6,51 +6,47 @@ import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
   langs: string[] = [];
-  detailPage:Boolean = false;
+  detailPage: Boolean = false;
 
-  constructor(private translate: TranslateService, private route: ActivatedRoute, private _location: Location, private router: Router) {
+  constructor(
+    private translate: TranslateService,
+    private route: ActivatedRoute,
+    private _location: Location,
+    private router: Router
+  ) {
     this.translate.setDefaultLang('en');
     this.translate.use('es');
     this.translate.addLangs(['es', 'en']);
     this.langs = this.translate.getLangs();
-    this.translate.stream('HELLO')
-    .subscribe((res: string) => {
-    });
+    this.translate.stream('HELLO').subscribe((res: string) => {});
     this.translate.stream('HOME').subscribe((res: string) => {});
 
-    router.events.subscribe((val) =>{
-      if(window.location.href.includes('characters/')) {
-        this.detailPage= true;
-      } else if (window.location.href.includes('houses/')){
-        this.detailPage= true;
-      }
-      else {
+    router.events.subscribe((val) => {
+      if (window.location.href.includes('characters/')) {
+        this.detailPage = true;
+      } else if (window.location.href.includes('houses/')) {
+        this.detailPage = true;
+      } else {
         this.detailPage = false;
       }
-      })
-
+    });
   }
 
-  changeLang(lang: string){
+  changeLang(lang: string) {
     this.translate.use(lang);
-    }
-
-    changeToEnglish(){
-      this.translate.use('en');
-    }
- 
-    goBack(){
-      this._location.back();
-    }
-   
-  ngOnInit(): void {
   }
+
+  changeToEnglish() {
+    this.translate.use('en');
+  }
+
+  goBack() {
+    this._location.back();
+  }
+
+  ngOnInit(): void {}
 }
-
-
-
--- src/app/pages/characters-page/characters-page.component.ts
+++ src/app/pages/characters-page/characters-page.component.ts
@@ -4,19 +4,18 @@ import { CharactersService } from 'src/app/shared/Services/characters.service';
 @Component({
   selector: 'app-characters-page',
   templateUrl: './characters-page.component.html',
-  styleUrls: ['./characters-page.component.scss']
+  styleUrls: ['./characters-page.component.scss'],
 })
 export class CharactersPageComponent implements OnInit {
-
   characters: any;
+  titleClass: string = 'c-gallery__title--hide';
 
-  constructor(private charactersService: CharactersService) { }
+  constructor(private charactersService: CharactersService) {}
 
   ngOnInit(): void {
     this.charactersService.getCharacters().subscribe((charactersData: any) => {
       console.log(charactersData);
       this.characters = charactersData;
-    })
+    });
   }
-
 }
