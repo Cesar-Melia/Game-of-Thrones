8c8c9126017e5798f5ea353ced30cc1c52afbbd8
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-06-07 09:53:37 +0200

    houses with incorrect data on api discarded

--- src/app/pages/houses-page/houses-page.component.ts
+++ src/app/pages/houses-page/houses-page.component.ts
@@ -19,9 +19,28 @@ export class HousesPageComponent implements OnInit {
   ngOnInit(): void {
     this.housesService.getHouses().subscribe((housesData: any) => {
       this.houses = housesData.filter((house: any) => house.logoURL);
+      // this.houses = this.houses.filter((house: any) => {
+      //   console.log(house.name.toLowerCase());
+      //   return house.name.toLowerCase() != 'house amber';
+      // });
 
       this.finderService.currentMessage.subscribe((message) => {
         this.houses = housesData.filter((house: any) => house.logoURL);
+        this.houses = this.houses.filter((house: any) => {
+          console.log(house.name.toLowerCase());
+          return (
+            !house.name.toLowerCase().includes('amber') &&
+            !house.name.toLowerCase().includes('blackmont') &&
+            !house.name.toLowerCase().includes('botley') &&
+            !house.name.toLowerCase().includes('frost') &&
+            !house.name.toLowerCase().includes('wood') &&
+            !house.name.toLowerCase().includes('user') &&
+            !house.name.toLowerCase().includes('lothston') &&
+            !house.name.toLowerCase().includes('manwoody') &&
+            !house.name.toLowerCase().includes('qoherys') &&
+            !house.name.toLowerCase().includes('towers')
+          );
+        });
         this.houses = this.houses.filter((house: any) =>
           house.name.toLowerCase().includes(message.toLowerCase())
         );
