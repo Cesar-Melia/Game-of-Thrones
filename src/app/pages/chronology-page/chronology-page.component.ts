1fd5c76f90c86f9cf254ba7cc45a63beb0720b34
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-06-07 07:46:56 +0200

    Chronology age search added

--- src/app/pages/chronology-page/chronology-page.component.ts
+++ src/app/pages/chronology-page/chronology-page.component.ts
@@ -7,31 +7,38 @@ import { CharactersService } from 'src/app/shared/Services/characters.service';
   styleUrls: ['./chronology-page.component.scss'],
 })
 export class ChronologyPageComponent implements OnInit {
+  characters: any;
   charactersFiltered: any;
-
-  charactersAge: any[] = [];
+  age: number = 0;
 
   constructor(private charactersService: CharactersService) {}
 
   ngOnInit(): void {
     this.charactersService.getCharacters().subscribe((charactersData: any) => {
-      this.charactersFiltered = charactersData.filter((character: any) => {
+      this.characters = charactersData.filter((character: any) => {
         return character.age && character.age.age;
       });
 
-      this.charactersFiltered = this.charactersFiltered.sort(
-        (a: any, b: any) => {
-          if (a.age.age > b.age.age) {
-            return 1;
-          }
-          if (a.age.age < b.age.age) {
-            return -1;
-          }
-          return 0;
+      this.characters = this.characters.sort((a: any, b: any) => {
+        if (a.age.age > b.age.age) {
+          return 1;
         }
-      );
+        if (a.age.age < b.age.age) {
+          return -1;
+        }
+        return 0;
+      });
 
-      console.log('paco -->', this.charactersFiltered);
+      this.charactersFiltered = [...this.characters];
     });
+  }
+
+  ageFilter() {
+    this.charactersFiltered = [...this.characters];
+    if (this.age) {
+      this.charactersFiltered = this.characters.filter(
+        (character: any) => character.age.age == this.age
+      );
+    }
   }
 }
